07a5f8b8c5dffae57d90814077a0a4d3173557e4
Chris <tayuanchou@vt.edu>
2024-03-16 00:28:58 -0400

    created homepage
    added homepage using Django framework

---
+++ homepage/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, "homepage/index.html")
+
+
+def posts(request):
+    return render(request, "homepage/posts.html")
